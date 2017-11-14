7015944619cd70122d1ff2039e0157b579e57d1c
Brian Whitton <me@brianwhitton.com>
2017-11-14 16:49:19 -0500

    replace body with app body

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="App__headline">{this.props.headline || this.state.headline}</h1>
+
+        <p className="App__summary">{this.props.summary || this.state.summary}</p>
+
+        {(this.props.callToAction || this.state.callToAction) &&
+          <div className="App__footer">
+            <a href={this.props.url || this.state.url} target="_blank" rel="nofollow noopener" className="App__call-out">{this.props.callToAction || this.state.callToAction}</a>
+          </div>
+        }
+      
       </div>
     );
   }
