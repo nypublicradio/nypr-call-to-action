ea7d2ded562f593ca4cdab6e73aea4091fdbd366
Brian Whitton <me@brianwhitton.com>
2017-11-15 09:09:47 -0500

    no service workers at all :(

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import unregister from './registerServiceWorker';
-
 import queryString from 'query-string';
 
 const params = queryString.parse(window.location.search);
 
 ReactDOM.render(<App {...params} />, document.getElementById('root'));
-unregister();
