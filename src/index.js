978d575d13a7353f50b11709283377af9da83538
Brian Whitton <me@brianwhitton.com>
2017-11-14 19:13:35 -0500

    uhhhh don't use service workers to cache all assets

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import unregister from './registerServiceWorker';
 
 import queryString from 'query-string';
 
 const params = queryString.parse(window.location.search);
 
 ReactDOM.render(<App {...params} />, document.getElementById('root'));
-registerServiceWorker();
+unregister();
