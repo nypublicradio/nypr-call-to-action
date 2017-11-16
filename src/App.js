5e8f79c85c9ecbb21bdbc0f4293670c9541ce097
Brian Whitton <me@brianwhitton.com>
2017-11-16 18:59:55 -0500

    refactor App.js for summary field
    set state from passed in props
    detect previewMode if all the attrs are blank

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import pym from 'pym.js';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      headline: this.props.headline,
+      summary: this.props.summary,
+      url: this.props.url,
+      callToAction: this.props.callToAction
+    };
     new pym.Child({polling: 200});
   }
   
@@ -20,20 +25,29 @@ class App extends Component {
   }
   
   render() {
-    return (
-      <div className="App">
-        <h1 className="App__headline">{this.props.headline || this.state.headline}</h1>
+    let { headline, summary, callToAction, url } = this.state;
+    let previewMode = [headline, summary, callToAction, url].every(x => !x);
+    if (previewMode) {
+      return (
+        <div className="App">
+          <p className="App__placeholder">Fill out the fields and your preview will appear here</p>
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <h1 className="App__headline">{headline}</h1>
 
-        <p className="App__summary">{this.props.summary || this.state.summary}</p>
+          <p className="App__summary">{summary}</p>
 
-        {(this.props.callToAction || this.state.callToAction) &&
-          <div className="App__footer">
-            <a href={this.props.url || this.state.url} target="_blank" rel="nofollow noopener" className="App__call-out">{this.props.callToAction || this.state.callToAction}</a>
-          </div>
-        }
-      
-      </div>
-    );
+          {callToAction &&
+            <div className="App__footer">
+              <a href={url} target="_blank" rel="nofollow noopener" className="App__call-out">{callToAction}</a>
+            </div>
+          }
+        </div>
+      );
+    }
   }
   
   parse(data) {
