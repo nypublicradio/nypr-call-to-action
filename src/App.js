ec09d28a1b0285de967728ba36d7c997366072f6
Brian Whitton <me@brianwhitton.com>
2018-02-02 11:37:32 -0500

    simplify by extending WidgetBase

--- src/App.js
+++ src/App.js
@@ -1,64 +1,8 @@
-import React, { Component } from 'react';
-import getattr from 'safe-object';
+import React from 'react';
 import './App.css';
+import WidgetBase from 'nypr-widget-base';
 
-const THEMES = process.env.REACT_APP_THEMES;
-
-export default class CallToAction extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      headline: this.props.headline,
-      summary: this.props.summary,
-      url: this.props.url,
-      callToAction: this.props.callToAction,
-      styles: {}
-    };
-
-    let { embed } = props;
-    if (embed) {
-      embed.onMessage('incoming', this.listener);
-    }
-  }
-
-  componentDidMount() {
-    let { embed } = this.props;
-    if (embed) {
-      embed.sendMessage('mounted');
-    }
-  }
-
-  componentWillUnmount() {
-    let { embed } = this.props;
-    if (embed) {
-      embed.remove();
-    }
-  }
-
-  componentDidUpdate(props, { brand }) {
-    if (this.state.brand !== brand) {
-      this.updateBrand();
-    }
-  }
-
-  updateBrand() {
-    fetch(`${THEMES}/${this.state.brand}.json`)
-      .then(r => r.json())
-      .then(styles => this.setState({styles}));
-  }
-
-  style(path) {
-    let parts = path.split('.');
-    if (parts.length === 1) {
-      // get everything nested under this path
-      return this.state.styles[path];
-    } else {
-      // look for a specific path
-      let key = parts.slice(-1);
-      return { [key]: getattr(this.state, `styles.${path}`) };
-    }
-  }
-
+export default class CallToAction extends WidgetBase {
   render() {
     let { headline, summary, callToAction, url } = this.state;
     let previewMode = [headline, summary, callToAction, url].every(x => !x);
@@ -92,20 +36,5 @@ export default class CallToAction extends Component {
         </div>
       );
     }
-  }
-
-  parse(data) {
-    let message = {};
-    if (typeof data === 'string') {
-      try {
-        message = JSON.parse(data);
-      } catch(e) {/* Ignored */}
-    }
-    return message;
-  }
-
-  listener = data => {
-    let query = this.parse(data);
-    this.setState(query);
   }
 }
